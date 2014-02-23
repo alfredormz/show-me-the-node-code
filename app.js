6a6318855f40ecf4db1bc796f296ce2192a82512
Alfredo Ramírez <alfredormz@gmail.com>
2014-02-23 15:25:00 -0300

    Move the lookup function and the error handling to a module

--- app.js
+++ app.js
@@ -1,43 +1,12 @@
 var express = require('express'),
-    request = require('request'),
-    cheerio = require('cheerio');
+    lookup = require('./npm_repo').lookup,
+    error_handler = require('./npm_repo').error_handler;
 
 var app = express();
 
 app.use(express.logger('dev'));
 
-function extract_url(content){
-  var $ = cheerio.load(content);
-  return $(".metadata tr:contains('Repository') a").attr("href") ||
-         $(".metadata tr:contains('Homepage') a").attr("href");
-}
-
-function lookup(req, res, next){
-  var package_name = req.params.package_name,
-      npm_url = "https://www.npmjs.org/package/" + package_name;
-
-  request(npm_url, function(error, response, body){
-
-    if(!error){
-      var url = extract_url(body);
-
-      url ? res.redirect(301, url) :
-            next(new Error(package_name + " not found"));
-
-    } else {
-      next(error);
-    }
-
-  });
-
-}
-
-function lookup_error_handler(err, req, res, next){
-  console.log(err.message);
-  res.status(404).send("There was an error: " + err.message);
-}
-
-app.get("/:package_name", lookup, lookup_error_handler);
+app.get("/:package_name", lookup, error_handler);
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
---
+++ npm_repo/index.js
@@ -0,0 +1,35 @@
+var request = require('request'),
+    cheerio = require('cheerio');
+
+
+function extract_url(content){
+  var $ = cheerio.load(content);
+  return $(".metadata tr:contains('Repository') a").attr("href") ||
+         $(".metadata tr:contains('Homepage') a").attr("href");
+}
+
+exports.lookup = function(req, res, next){
+  var package_name = req.params.package_name,
+      npm_url = "https://www.npmjs.org/package/" + package_name;
+
+  request(npm_url, function(error, response, body){
+
+    if(!error){
+      var url = extract_url(body);
+
+      url ? res.redirect(301, url) :
+            next(new Error("repo " + package_name + " not found"));
+
+    } else {
+      next(error);
+    }
+
+  });
+
+}
+
+exports.error_handler = function(err, req, res, next){
+  console.log(err.message);
+  res.status(404).send("There was an error: " + err.message);
+}
+
