b0dd751e2602a94e594a21ae2b71c534c51f2099
Alfredo Ramírez <alfredormz@gmail.com>
2014-02-23 13:42:38 -0300

    Extract URL logic to a method

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ var app = express();
 
 app.use(express.logger('dev'));
 
+function extract_url(content){
+  var $ = cheerio.load(content);
+  return $(".metadata tr:contains('Repository') a").attr("href") ||
+         $(".metadata tr:contains('Homepage') a").attr("href");
+}
+
 function lookup(req, res, next){
   var package_name = req.params.package_name,
       npm_url = "https://www.npmjs.org/package/" + package_name;
 
   request(npm_url, function(error, response, body){
 
-    if(!error && response.statusCode == 200){
-      var $ = cheerio.load(body);
-      var url = $(".metadata tr:contains('Repository') a").attr("href") ||
-                $(".metadata tr:contains('Homepage') a").attr("href");
+    if(!error){
+      var url = extract_url(body);
 
       url ? res.redirect(301, url) :
             res.status(404).send("Repo for " + package_name + " not found");
