4298c4a45e8102ac16437aa3518d27b685827aa4
Alfredo Ramírez <alfredormz@gmail.com>
2014-02-25 22:57:57 -0300

    Add some memoization, bitches love memoization.
    Add a memoization function in order to avoid redundant requests and html parsing

--- npm_repo/index.js
+++ npm_repo/index.js
@@ -1,6 +1,7 @@
 var request = require('request'),
     cheerio = require('cheerio');
 
+var simple_cache = exports.simple_cache = {};
 
 function extract_url(content){
   var $ = cheerio.load(content);
@@ -12,14 +13,22 @@ exports.lookup = function(req, res, next){
   var package_name = req.params.package_name,
       npm_url = "https://www.npmjs.org/package/" + package_name;
 
+  if(simple_cache[package_name]){
+    console.log("Cache hit for " + package_name);
+    res.redirect(simple_cache[package_name]);
+    return
+  }
+
   request(npm_url, function(error, response, body){
 
     if(error) { next(error); }
 
     var url = extract_url(body);
-    url ? res.redirect(301, url) :
-          next(new Error("repo " + package_name + " not found"));
 
+    console.log("Cache miss for " + package_name);
+    simple_cache[package_name] = url;
+    url ? res.redirect(url) :
+          next(new Error("repo " + package_name + " not found"));
   });
 }
 
