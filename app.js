0f370c21634caaf93fe257c863ebc6124cf91695
Alfredo Ramírez <alfredormz@gmail.com>
2014-03-02 12:00:38 -0300

    Fixed the israeli-palestinian conflict

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require('express'),
-    lookup = require('./npm_repo').lookup,
-    error_handler = require('./npm_repo').error_handler;
+    lookup = require('./npm_repo').lookup;
 
 var app = express();
 
 app.use(express.logger('dev'));
 
-app.get("/:package_name", lookup, error_handler);
+function errorHandler(err, req, res, next){
+  console.log(err.message);
+  res.status(404).send("There was an error: " + err.message);
+}
+
+app.get("/:packageName", lookup, errorHandler);
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
--- npm_repo/index.js
+++ npm_repo/index.js
@@ -1,39 +1,35 @@
 var request = require('request'),
     cheerio = require('cheerio');
 
-var simple_cache = exports.simple_cache = {};
+var _cache = exports._cache = {};
+
+exports.lookup = function(req, res, next){
+  var packageName = req.params.packageName,
+      npm_url = "https://www.npmjs.org/package/" + packageName;
+
+  if(!_cache[packageName]) {
+    console.log("Cache missing for " + packageName);
+
+    request(npm_url, function(error, response, body){
+      if(error) { next(error); }
+
+      var url = extract_url(body);
+      if(url){
+        _cache[packageName] = url;
+        res.redirect(url);
+      } else {
+        next(new Error("repo " + packageName + " not found"));
+      }
+    });
+
+  } else {
+    console.log("Cache hit for " + packageName);
+    res.redirect(_cache[packageName]);
+  }
+}
 
 function extract_url(content){
   var $ = cheerio.load(content);
   return $(".metadata tr:contains('Repository') a").attr("href") ||
          $(".metadata tr:contains('Homepage') a").attr("href");
 }
-
-exports.lookup = function(req, res, next){
-  var package_name = req.params.package_name,
-      npm_url = "https://www.npmjs.org/package/" + package_name;
-
-  if(simple_cache[package_name]){
-    console.log("Cache hit for " + package_name);
-    res.redirect(simple_cache[package_name]);
-    return
-  }
-
-  request(npm_url, function(error, response, body){
-
-    if(error) { next(error); }
-
-    var url = extract_url(body);
-
-    console.log("Cache miss for " + package_name);
-    simple_cache[package_name] = url;
-    url ? res.redirect(url) :
-          next(new Error("repo " + package_name + " not found"));
-  });
-}
-
-exports.error_handler = function(err, req, res, next){
-  console.log(err.message);
-  res.status(404).send("There was an error: " + err.message);
-}
-
